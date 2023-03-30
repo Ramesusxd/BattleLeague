8adfa5824db551d422de754f663b4df18349e0b9
RamesusXD <j1gamer4life@gmail.com>
2023-03-29 21:05:14 -0700

    updatedcolortheme

--- App.js
+++ App.js
@@ -5,10 +5,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-<<<<<<< HEAD
   TouchableOpacity,
-=======
->>>>>>> origin/master
   useColorScheme,
   View,
 } from 'react-native';
@@ -17,30 +14,35 @@ const redColor = '#FF0000';
 const whiteColor = '#FFFFFF';
 const blackColor = '#000000';
 
-const CustomHeader = ({title}) => {
+// CustomHeader component
+const CustomHeader = ({ title }) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={[styles.headerContainer, {backgroundColor: isDarkMode ? blackColor : whiteColor}]}>
-      <Text style={[styles.headerTitle, {color: isDarkMode ? whiteColor : redColor}]}>
-        {title}
-      </Text>
+    <View style={[styles.headerContainer, { backgroundColor: isDarkMode ? blackColor : whiteColor }]}>
+      <Text style={[styles.headerTitle, { color: isDarkMode ? whiteColor : redColor }]}>{title}</Text>
     </View>
   );
 };
 
-<<<<<<< HEAD
-const Section = ({children, title, last}) => {
+// Section component
+const Section = ({ children, title, last }) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <TouchableOpacity onPress={() => alert(`You clicked ${title}`)}>
-      <View style={[styles.sectionContainer, last ? {borderBottomWidth: 0} : {borderBottomWidth: 1}]}>
+      <View
+        style={[
+          styles.sectionContainer,
+          { borderBottomWidth: last ? 0 : 1 }, // Update border bottom width based on 'last' prop
+        ]}
+      >
         <Text
           style={[
             styles.sectionTitle,
             {
               color: isDarkMode ? whiteColor : redColor,
             },
-          ]}>
+          ]}
+        >
           {title}
         </Text>
         <Text
@@ -49,39 +51,16 @@ const Section = ({children, title, last}) => {
             {
               color: isDarkMode ? blackColor : whiteColor,
             },
-          ]}>
+          ]}
+        >
           {children}
         </Text>
       </View>
     </TouchableOpacity>
-=======
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? whiteColor : redColor,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? blackColor : whiteColor,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
->>>>>>> origin/master
   );
 };
 
+// App component
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -95,36 +74,27 @@ const App = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
         <CustomHeader title="Trainer Battle League" />
         <View
           style={{
             backgroundColor: isDarkMode ? whiteColor : blackColor,
-          }}>
+          }}
+        >
           <Section title="Trainer Profile">
-            See your <Text style={styles.highlight}>Trainer Profile</Text> and accomplishments across 
+            See your <Text style={styles.highlight}>Trainer Profile</Text> and accomplishments across
             the different battle communities.
           </Section>
           <Section title="Battle Communities">
-            Browse New Trainer communities or participate 
-            in your favorite one.
+            Browse New Trainer communities or participate in your favorite one.
           </Section>
           <Section title="Badge Book">
-            See your badges you earned in different Trainer Communities
-            are you ready for the pokemon league yet.
+            See your badges you earned in different Trainer Communities. Are you ready for the
+            Pokemon League yet?
           </Section>
-<<<<<<< HEAD
           <Section title="Settings" last={true}>
             Change your settings
           </Section>
-=======
-          <Section title="Settings">
-            Change your settings
-          </Section>
-          
->>>>>>> origin/master
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -146,14 +116,10 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
     marginBottom: 16,
-<<<<<<< HEAD
-    borderBottomWidth: 1,
     borderBottomColor: whiteColor,
     marginLeft: 16,
     marginRight: 16,
     paddingBottom: 12,
-=======
->>>>>>> origin/master
   },
   sectionTitle: {
     fontSize: 24,
@@ -171,7 +137,4 @@ const styles = StyleSheet.create({
 
 export default App;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
